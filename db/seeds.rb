191f8554e24850bd443c2f330a37f8f23f31f3e2
Alex Latham <alexlthm@gmail.com>
2020-03-18 15:29:38 -0600

    Add columns for location in shelters table, update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-shelter1 = Shelter.create(name:"Ripley's Playhouse")
-shelter2 = Shelter.create(name:"Camp Bow Wow")
-shelter3 = Shelter.create(name:"Camp Bug Juice")
+shelter1 = Shelter.create(name:     "Dumb Friends League",
+                          address:  "2080 S. Quebec St.",
+                          city:     "Denver",
+                          state:    "CO",
+                          zip:      "80231")
+shelter2 = Shelter.create(name:     "Denver Animal Shelter",
+                          address:  "1241 W. Bayaud Ave.",
+                          city:     "Denver",
+                          state:    "CO",
+                          zip:      "80223")
+shelter3 = Shelter.create(name:     "MaxFund Animal Adoption Center",
+                          address:  "1005 Galapago St.",
+                          city:     "Denver",
+                          state:    "CO",
+                          zip:      "80204")
+shelter4 = Shelter.create(name:     "Foothills Animal Shelter",
+                          address:  "580 McIntyre St.",
+                          city:     "Golden",
+                          state:    "CO",
+                          zip:      "80401")
