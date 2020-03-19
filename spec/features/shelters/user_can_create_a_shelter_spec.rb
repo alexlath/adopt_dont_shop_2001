5886cb32f0613b730fe06491da990bf65f21c3d2
Alex Latham <alexlthm@gmail.com>
2020-03-18 18:17:31 -0600

    Add form to submit new shelter

--- spec/features/shelters/user_can_create_a_shelter_spec.rb
+++ spec/features/shelters/user_can_create_a_shelter_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
-  it "can show create a new shelter" do
+  it "can create a new shelter" do
+
+    visit "/shelters"
+    expect(page).to have_content("New Shelter")
+
+    click_on 'New Shelter'
+    expect(page).to have_current_path "/shelters/new"
+
+    fill_in 'shelter[name]', with: "Testing1"
+    fill_in 'shelter[address]', with: "Testing2"
+    fill_in 'shelter[city]', with: "Testing3"
+    fill_in 'shelter[state]', with: "Testing4"
+    fill_in 'shelter[zip]', with: "Testing5"
+    click_on 'Create Shelter'
+
+    expect(page).to have_content("Testing1")
   end
 end
