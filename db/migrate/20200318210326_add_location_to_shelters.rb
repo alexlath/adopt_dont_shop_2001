b6dd7009578902c877f6b1a537513ebd3a44064d
Alex Latham <alexlthm@gmail.com>
2020-03-21 16:25:56 -0600

    Reset database and migration

--- db/migrate/20200318210326_add_location_to_shelters.rb
+++
@@ -1,8 +0,0 @@
-class AddLocationToShelters < ActiveRecord::Migration[5.1]
-  def change
-    add_column :shelters, :address, :string
-    add_column :shelters, :city, :string
-    add_column :shelters, :state, :string
-    add_column :shelters, :zip, :string
-  end
-end
