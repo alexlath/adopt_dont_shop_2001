75e49fe4f3cd5292040e05ab3774cad5f8c49b42
Alex Latham <alexlthm@gmail.com>
2020-03-21 21:55:45 -0600

    Update pet tests to have pet description

--- spec/features/pets/user_can_see_all_pets_spec.rb
+++ spec/features/pets/user_can_see_all_pets_spec.rb
@@ -8,11 +8,24 @@ RSpec.describe "As a visitor", type: :feature do
                              city:    "Denver",
                              state:   "CO",
                              zip:     "80231")
-    pet = shelter.pets.create(image:      "kona.jpeg",
-                              name:       "Kona",
-                              approx_age: 6,
-                              sex:        "Male")
-    
+
+    pet = shelter.pets.create(image:        "kona.jpeg",
+                              name:         "Kona",
+                              description:  "Kona greets everyone with the \
+                                            biggest smile! He's always happy \
+                                            and is so easy to fall in love \
+                                            with. He seems to love everyone he \
+                                            meets, but can get a little overly \
+                                            excited some times and may knock \
+                                            little kids down. He is reportedly \
+                                            housebroken and does well when \
+                                            left alone in the home. He would \
+                                            benefit from daily walks and lots \
+                                            of playtime!",
+                              approx_age:   6,
+                              sex:          "Male",
+                              status:       "Adoptable")
+
     visit "/pets"
 
     expect(page).to have_xpath("//img[contains(@src, #{pet.image})]")
